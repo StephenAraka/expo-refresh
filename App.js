15fb9df096f4cd3e8dcbb991cb9cb1462d23fc45
Stephen Araka <stephen@Stephens-MacBook-Pro.local>
2024-09-24 23:45:26 +0300

    Move image to its component

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Image, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+
+ import ImageViewer from './components/ImageViewer'; 
 
 const PlaceholderImage = require('./assets/images/background-image.png');
 
@@ -7,7 +9,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <Image source={PlaceholderImage} style={styles.image} />
+        <ImageViewer placeholderImageSource={PlaceholderImage} />
       </View>
       <StatusBar style="auto" />
     </View>
@@ -19,6 +21,9 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
   },
 });
